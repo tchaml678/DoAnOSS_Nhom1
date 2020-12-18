bc66327db1cc14bdfd87a33ee588837e8ccd75b3
BichNhuy <163.nhuy@gmail.com>
2020-12-18 22:03:02 +0700

    bullet

---
+++ 2DSpaceShooter/Assets/Script/BossHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+    private Bullet damageBullet;
+    [SerializeField] GameObject bossBang;
+    [SerializeField] int bossHP ;
+    [SerializeField] int bodyDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            damageBullet = collision.gameObject.GetComponent<Bullet>();
+            bossHP -= damageBullet.Damage();
+            if (bossHP <= 0)
+            {
+                var onBossBang = Instantiate(bossBang, transform.position, transform.rotation);
+                Destroy(onBossBang, 2.6f);
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+    public int Damage()
+    {
+        return bodyDamage;
+    }
+}
---
+++ 2DSpaceShooter/Assets/Script/CreateEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefabs;
+    [SerializeField] float delayTime = 10f;
+    private float counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Create();
+    }
+
+    void Create()
+    {
+        
+        var enemyPos = transform.position;
+        enemyPos.y += 0.5f;
+        counter += Time.deltaTime*Random.Range(0.1f,0.5f);
+        if (counter >= delayTime)
+        {
+            Instantiate(enemyPrefabs, enemyPos, transform.rotation);
+            counter = 0;
+        }
+    }
+}
+
